bdaa228922f98daf0ca6a647a562a2d9b8d4ce9c
stvjbz <stvjbz@outlook.com>
2016-05-06 01:37:19 +0900

    [add] an AnbayashiData class file.

--- app/src/main/java/com/stvjbz/sampleapp03/AnbayashiData.java
+++ app/src/main/java/com/stvjbz/sampleapp03/AnbayashiData.java
@@ -4,4 +4,25 @@ package com.stvjbz.sampleapp03;
  * Created by stvjbz on 16/05/06.
  */
 public class AnbayashiData {
+    private int number;
+    private int addition;
+    private String comment;
+    //Constructor
+    public AnbayashiData(int number, int addition, String comment) {
+        this.number = number;
+        this,addition = addition;
+        this.comment = comment;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getAddition() {
+        return addition;
+    }
+
+    public String getComment() {
+        return comment;
+    }
 }
