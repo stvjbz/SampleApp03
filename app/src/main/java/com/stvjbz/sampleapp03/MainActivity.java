e72cb4b964c44910b3115e9c8463ae5b72d5563d
stvjbz <stvjbz@outlook.com>
2016-05-10 01:24:50 +0900

    [add] メインアクティビティにRecyclerViewオブジェクトを設定.

--- app/src/main/java/com/stvjbz/sampleapp03/MainActivity.java
+++ app/src/main/java/com/stvjbz/sampleapp03/MainActivity.java
@@ -2,6 +2,10 @@ package com.stvjbz.sampleapp03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.cardList);
+
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager llManager = new LinearLayoutManager(this);
+        // 縦スクロール
+        llManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        ArrayList<AnbayashiData> anbayashi = new ArrayList<AnbayashiData>();
+        for (int i = 0; i < MyData.commentArray.length; i++) {
+            anbayashi.add(new AnbayashiData(
+                    MyData.numberArray[i],
+                    MyData.additionArray[i],
+                    MyData.commentArray[i]
+            ));
+        }
+
+        RecyclerView.Adapter adapter = new AnbayashiAdapter(anbayashi);
+        recyclerView.setAdapter(adapter);
+        recyclerView.smoothScrollToPosition(anbayashi.size() - 1); // 最後までスクロール
     }
 }
