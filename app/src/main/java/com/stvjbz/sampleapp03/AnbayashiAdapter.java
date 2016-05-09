8cc1cc78dc59de7f83b29cb5e2865198419acd41
stvjbz <stvjbz@outlook.com>
2016-05-10 02:06:17 +0900

    [add] ViewHolder,Adapterクラスを追加.

--- app/src/main/java/com/stvjbz/sampleapp03/AnbayashiAdapter.java
+++ app/src/main/java/com/stvjbz/sampleapp03/AnbayashiAdapter.java
@@ -1,7 +1,57 @@
 package com.stvjbz.sampleapp03;
 
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 /**
  * Created by stvjbz on 16/05/06.
  */
-public class AnbayashiAdapter {
+public class AnbayashiAdapter extends RecyclerView.Adapter<AnbayashiViewHolder> {
+    private ArrayList<AnbayashiData> rouletteDataSet;
+
+    public AnbayashiAdapter(ArrayList<AnbayashiData> roulette) {
+
+        this.rouletteDataSet = roulette;
+    }
+    // 新しいViewを作成する
+    // レイアウトマネージャーにより起動される
+    @Override
+    public AnbayashiViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // parentはRecyckerView
+        // public View inflate (int resource, ViewGroup root, boolean attachToRoot)
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cards_layout, parent, false);
+        Log.d("Anbayashi", parent.toString());
+
+        //AnbayashiViewHolder anbayashiViewHolder = new AnbayashiViewHolder(view);
+        return new AnbayashiViewHolder(view);
+    }
+    // Viewの内容を交換する（リサイクルだから）
+    // レイアウトマネージャーにより起動される
+    @Override
+    public void onBindViewHolder(final AnbayashiViewHolder holder, final int listPosition) {
+
+        holder.textViewNumber.setText(rouletteDataSet.get(listPosition).getNumber()+ R.string.book);
+        holder.textViewComment.setText(rouletteDataSet.get(listPosition).getComment());
+        holder.base.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // vはCardView
+                Toast.makeText(v.getContext(),R.string.present +
+                    rouletteDataSet.get(listPosition).getAddition() + R.string.book, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    @Override
+    public int getItemCount() {
+
+        return rouletteDataSet.size();
+    }
 }
--- app/src/main/java/com/stvjbz/sampleapp03/AnbayashiViewHolder.java
+++ app/src/main/java/com/stvjbz/sampleapp03/AnbayashiViewHolder.java
@@ -1,7 +1,22 @@
 package com.stvjbz.sampleapp03;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by stvjbz on 16/05/06.
  */
-public class AnbayashiViewHolder {
+public class AnbayashiViewHolder extends RecyclerView.ViewHolder {
+
+    View base;
+    TextView textViewNumber;
+    TextView textViewComment;
+
+    public  AnbayashiViewHolder(View v) {
+        super(v);
+        this.base = v;
+        this.textViewNumber = (TextView) v.findViewById(R.id.number);
+        this.textViewComment = (TextView) v.findViewById(R.id.comment);
+    }
 }
